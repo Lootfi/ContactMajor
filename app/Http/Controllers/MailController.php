6c7e735006723df99a1507773ee9e84fe7b0c53e
Lootfi <lotfiholmes@hotmail.com>
2020-07-26 10:52:04 +0100

    verify email template & confirm payment middleware

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -6,6 +6,7 @@ use App\User;
 use Illuminate\Http\Request;
 use Socketlabs\SocketLabsClient;
 use Socketlabs\Message\BasicMessage;
+use Socketlabs\Message\BulkMessage;
 use Socketlabs\Message\EmailAddress;
 
 
@@ -31,5 +32,28 @@ class MailController extends Controller
         $message->addBccAddress($recipient->email, $recipient->name);
 
         $response = $this->socketlabsClient->send($message);
+    }
+
+    public function send_via_request(Request $request)
+    {
+
+        try {
+            $message = new BulkMessage();
+
+            $message->subject = $request->subject;
+            $message->htmlBody = file_get_contents(__DIR__ . '\..\..\..\public\verify-email-template.html');
+            $message->addGlobalMergeData("recipient", $request->name);
+            $message->plainTextBody = $request->plainBody;
+
+            $message->from = new EmailAddress("contact@contactmajor.com");
+            $message->addToAddress($request->email, $request->name);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+
+
+        $response = $this->socketlabsClient->send($message);
+
+        return response()->json($response);
     }
 }
